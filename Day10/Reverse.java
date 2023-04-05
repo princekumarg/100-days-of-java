4fbdcce098d004604ad67b8c63865b3e5b1b2811
prince kumar <agarwal8789@gmail.com>
2023-04-05 22:15:13 +0530

    3rd commit

--- Day10/Reverse.java
+++ Day10/Reverse.java
@@ -3,5 +3,15 @@ import java.util.*;
 public class Reverse {
     public static void main(String args[]) {
         Scanner s=new Scanner(System.in);
+        String str=s.nextLine();
+        String rev=reverse(str);
+        System.out.println(rev);    
+    }
+    public static String reverse(String str) {
+        String revstr=" ";
+        for(int i=str.length()-1;i>=0;i++){
+            revstr=revstr+str.charAt(i);
+        }
+        return revstr;
     }
 }
