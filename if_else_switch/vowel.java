75ec533098a1765ba0e39c467aae1c4d390115c6
prince kumar <agarwal8789@gmail.com>
2023-04-24 17:15:56 +0530

    2nd commit made

---
+++ if_else_switch/vowel.java
@@ -0,0 +1,15 @@
+package if_else_switch;
+import java.util.*;
+public class vowel {
+    public static void main(String args[]) {
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter the charcter=");
+        char c=s.next().charAt(0);
+        if(c=='a'||c=='A'||c=='e'||c=='E'||c=='i'||c=='I'||c=='o'||c=='O'){
+            System.out.println("vowel");
+        }
+        else{
+            System.out.println("consonent");
+        }
+    }
+}
