ecb04d303d758ed56525b4ff7db7b951ec151afc
prince kumar <agarwal8789@gmail.com>
2023-04-27 21:55:46 +0530

    5th commit made

--- Mathmatics/gcd_of_two.java
+++ Mathmatics/gcd_of_two.java
@@ -11,11 +11,22 @@ public class gcd_of_two {
         }
         return res;
     }
+    public static int lcm(int a,int b) {
+        int res=Math.max(a,b);
+        while(true){
+            if(res%a==0 && res%b==0){
+                break;
+            }
+            res++;
+        }
+        return res;
+    }
     public static void main(String args[]) {
         Scanner s=new Scanner(System.in);
         System.out.println("Enter your numbers");
         int a=s.nextInt();
         int b=s.nextInt();
         System.out.println(gcd(a,b));
+        System.out.println(lcm(a,b));
     }
 }
