ce4d1b0b5a35e8002dbfebb636c3de5df92167bb
prince kumar <agarwal8789@gmail.com>
2023-05-05 09:06:25 +0530

    7th commit made

--- Recursion/even_odd.java
+++ Recursion/even_odd.java
@@ -6,7 +6,7 @@ public class even_odd {
         System.out.print("Enter a number=");
         int n=s.nextInt();
         System.out.println(even(n));
-        System.out.println("odd="+odd(n));
+        System.out.println("Even="+odd(n));
     }
     public static int even(int n) {
         if(n==0){
@@ -16,6 +16,14 @@ public class even_odd {
             System.out.println(n);
             return even(n-2);
         }
-        
+    }
+    public static int odd(int n) {
+        if(n==1){
+            return 1;
+        }
+        else{
+            System.out.println(n);
+            return odd(n-2);
+        }
     }
 }
