6b37eebb002807bf0a4e590b4b9fd8ace9918902
prince kumar <agarwal8789@gmail.com>
2023-05-07 11:50:11 +0530

    8th commit made

---
+++ TypeConversion/binary_to_decimal.java
@@ -0,0 +1,9 @@
+package TypeConversion;
+import java.util.*;
+public class binary_to_decimal {
+    public static void main(String args[]) {
+        String binaryString="1010";  
+        int decimal=Integer.parseInt(binaryString,2);  
+        System.out.println(decimal);  
+    }
+}
---
+++ TypeConversion/operator.java
@@ -0,0 +1,15 @@
+package TypeConversion;
+import java.util.*;
+public class operator {
+    public static void main(String args[]) {
+        System.out.println("a" + "b");
+        System.out.println((char)('a' + 3));
+
+        System.out.println("a" + 1);
+        System.out.println("prince" + new ArrayList<>());
+        System.out.println("prince" + new Integer(56));
+        String ans = new Integer(56) +" "+ new ArrayList<>();
+        System.out.println(ans);
+        System.out.println("a" + 'b');
+    }
+}
