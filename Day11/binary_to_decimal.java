7ad39971eef2508fc4c7e1d7323868ab52719a72
PRINCE KUMAR <86905668+princekumarg@users.noreply.github.com>
2023-05-07 11:46:39 +0530

    Delete Day11 directory

--- Day11/binary_to_decimal.java
+++
@@ -1,9 +0,0 @@
-package Day11;
-import java.util.*;
-public class binary_to_decimal {
-    public static void main(String args[]) {
-        String binaryString="1010";  
-        int decimal=Integer.parseInt(binaryString,2);  
-        System.out.println(decimal);  
-    }
-}
