fa769425d964ae2f15a69003dccf6bb8d0712e50
prince kumar <agarwal8789@gmail.com>
2023-06-13 23:56:30 +0530

    commit made

--- String/NonReption.java
+++ String/NonReption.java
@@ -1,5 +1,5 @@
 package String;
-
+import java.util.*;
 public class NonReption {
     public static int NonRep(String str) {
         for(int i=0;i<str.length();i++){
@@ -16,8 +16,41 @@ public class NonReption {
         }
         return -1;
     }
+    public static int NonRep1(String str) {
+        int []count=new int[256];
+        for(int i=0;i<str.length();i++){
+            count[str.charAt(i)]++;
+        }
+        for(int i=0;i<str.length();i++){
+            if(count[str.charAt(i)]==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static int NonRep2(String str) {
+        int fi[]=new int[256];
+        Arrays.fill(fi,-1);
+        for(int i=0;i<str.length();i++){
+            if(fi[str.charAt(i)]==-1){
+                fi[str.charAt(i)]=i;
+            }
+            else{
+                fi[str.charAt(i)]=-2;
+            }
+        }
+        int res=Integer.MAX_VALUE;
+        for(int i=0;i<256;i++){
+            if(fi[i]>=0){
+                res=Math.min(res,fi[i]);
+            }
+        }
+        return (res==Integer.MAX_VALUE)?-1:res;
+    }
     public static void main(String[] args) {
         String str="geeksforgeeks";
         System.out.println(NonRep(str));
+        System.out.println(NonRep1(str));
+        System.out.println(NonRep2(str));
     }
 }
