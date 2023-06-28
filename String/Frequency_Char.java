54ad92af74ee339a0e112224b4b4d200ec3796de
prince kumar <agarwal8789@gmail.com>
2023-06-28 19:08:11 +0530

    16TH commit

--- String/Frequency_Char.java
+++ String/Frequency_Char.java
@@ -1,8 +1,11 @@
 package String;
+
 import java.util.*;
+
 public class Frequency_Char {
     static int SIZE = 26;
-    public static void printCharWithFreq(String str){
+
+    public static void printCharWithFreq(String str) {
         int n = str.length();
         int[] freq = new int[SIZE];
         for (int i = 0; i < n; i++)
@@ -15,11 +18,26 @@ public class Frequency_Char {
                 freq[str.charAt(i) - 'a'] = 0;
             }
         }
- 
     }
+
+    public static int firstUniqChar(String s) {// find the first unique Char in string
+        int n = s.length();
+        int[] str = new int[26];
+        for (int i = 0; i < n; i++)
+            str[s.charAt(i) - 'a']++;
+        for (int i = 0; i < n; i++) {
+            if (str[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String args[]) {
-        String str = "geeksforgeeks";
-        printCharWithFreq(str);
+        // String str = "geeksforgeeks";
+        String s = "Leetcode";
+        // printCharWithFreq(str);
+        System.out.println(firstUniqChar(s));
     }
 
 }
