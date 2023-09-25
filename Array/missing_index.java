eba48057c1641b564e5178d56ded944e4de69b9d
PRINCE KUMAR <86905668+princekumarg@users.noreply.github.com>
2023-09-25 15:39:13 +0530

    Update missing_index.java

--- Array/missing_index.java
+++ Array/missing_index.java
@@ -17,4 +17,15 @@ public class missing_index {
         int sum=(n+1)*(n+2)/2;
         System.out.println("The Missing is"+(sum-total));
     }
+    public static int missing(int arr[]) {
+        int x1=arr[0];
+        int x2=1;
+        for(int i=1;i<arr.length;i++) {
+            x1=x1^arr[i];
+        }
+        for(int i=2;i<arr.length;i++) {
+            x2=x2^i;
+        }
+        return (x2^x1) ;
+        
 }
