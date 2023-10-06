39132a75037d0818c278eb61fcabd5d05ce87f10
prince kumar <agarwal8789@gmail.com>
2023-10-07 00:27:50 +0530

    commit made by prince"

--- DP/MaximumsumNon.java
+++ DP/MaximumsumNon.java
@@ -17,9 +17,24 @@ public class MaximumsumNon {
         return prev;
     }
 
+    public static int robber(int n, int arr[]) {
+        int arr1[] = new int[n];
+        int arr2[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (i != 0) {
+                arr1[i] = arr[i];
+            }
+            if (i != n - 1) {
+                arr2[i] = arr[i];
+            }
+        }
+        return Math.max(maxsum(n, arr2), maxsum(n, arr1));
+    }
+
     public static void main(String[] args) {
         int arr[] = { 5, 10, 10, 100, 5, 6 };
         int n = arr.length;
         System.out.println(maxsum(n, arr));
+        System.out.println(robber(n, arr));
     }
 }
--- Searching/sqrt.java
+++ Searching/sqrt.java
@@ -1,34 +1,87 @@
 package Searching;
+
 import java.util.*;
+
 public class sqrt {
     public static int sqrt(int n) {
-        int i=1;
-        while(i*i<=n){
+        int i = 1;
+        while (i * i <= n) {
             i++;
         }
-        return i-1;
+        return i - 1;
     }
-    public static int sqrt1(int n) {//for timeexceed error put start datatypes as long
-        int start=0,end=n,ans=-1;//for validation in boolean put boolean ans=false
-        while(start<=end){
-            int mid=(start+end)/2;
-            int midsq=mid*mid;
-            if(midsq==n){
-                return mid;//return as true;
-            }
-            else if(midsq>n){
-                end=mid-1;
-            }
-            else{
-                start=mid+1;
-                ans=mid;//in validation case remove the ans 
+
+    public static int sqrt1(int n) {// for timeexceed error put start datatypes as long
+        int start = 1, end = n;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (mid * mid <= n) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
-        return ans;
+        return end;
     }
+
+    public static int sqrt2(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int start = 1, end = n;
+        int mid = -1;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (mid * mid == n) {
+                return mid;
+            }
+            if (mid * mid < n) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+
+        }
+        return Math.round(mid);
+
+    }
+
+    public static int solve(int mid, int n, int m) {
+        long ans = 1;
+        for (int i = 1; i <= n; i++) {
+            ans = ans * mid;
+            if (ans > m) {
+                return 2;
+            }
+        }
+        if (ans == m) {
+            return 1;
+        }
+        return 0;
+
+    }
+
+    public static int sqrtatNth(int n, int m) {
+        int start = 1, end = m;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            int res = solve(mid, end, mid);
+            if (res == 1) {
+                return mid;
+            }
+            if (res == 0) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String args[]) {
-        int n=10;
+        int n = 10;
         System.out.println(sqrt(n));
         System.out.println(sqrt1(n));
+        System.out.println(sqrt2(n));
     }
 }
