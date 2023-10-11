9296513073a74085da660e3739bf603cfd81c981
prince kumar <agarwal8789@gmail.com>
2023-10-11 15:59:28 +0530

    commit made

---
+++ Arrays/majorityeleNBY2.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+public class majorityeleNBY2 {
+    public static int majority(int arr[]) {
+        int n = arr.length;
+        int cnt = 0;
+        int el = 0;
+        for (int i = 0; i < n; i++) {
+            if (cnt == 0) {
+                cnt = 1;
+                el = arr[i];
+            } else if (el == arr[i]) {
+                cnt++;
+            } else {
+                cnt--;
+            }
+        }
+        int cnt1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == el) {
+                cnt1++;
+            }
+        }
+        if (cnt1 > (n / 2)) {
+            return el;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 2, 1, 1, 1, 2, 2 };
+        System.out.println(majority(arr));
+    }
+}
