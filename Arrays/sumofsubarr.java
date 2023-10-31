82c606998d5d134443eb4e49abf975c2f4393767
prince kumar <agarwal8789@gmail.com>
2023-10-31 15:08:00 +0530

    commit made

--- Arrays/sumofsubarr.java
+++ Arrays/sumofsubarr.java
@@ -39,10 +39,31 @@ public class sumofsubarr {
         return maxlen;
     }
 
+    public static int sumzero(int arr[], int n) {
+        int sum = 0, maxi = 0;
+        Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
+        for (int i = 0; i < n; i++) {
+            sum = sum + arr[i];
+            if (sum == 0) {
+                maxi = Math.max(maxi, i + 1);
+            }
+            if (mp.containsKey(sum)) {
+                int len = i - mp.get(sum);
+                maxi = Math.max(maxi, len);
+            }
+            if (!mp.containsKey(sum)) {
+                mp.put(sum, i);
+            }
+        }
+        return maxi;
+    }
+
     public static void main(String[] args) {
         int arr[] = { 10, 5, 2, 7, 1, 9 };
         int n = arr.length;
         int k = 15;
         System.out.println(sumofsubarray(arr, n, k));
+        System.out.println(sumofsubarr1(arr, n, k));
+        System.out.println(sumzero(arr, n));
     }
 }
--- Matrix/PascalTri.java
+++ Matrix/PascalTri.java
@@ -1,4 +1,4 @@
-package Arrays;
+package Matrix;
 
 import java.util.ArrayList;
 import java.util.List;
