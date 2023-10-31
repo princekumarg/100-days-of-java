e9318557892331d2aaaf90c0c3930e1157186250
prince kumar <agarwal8789@gmail.com>
2023-10-31 15:28:24 +0530

    commit made

--- Arrays/alternativeelement.java
+++ Arrays/alternativeelement.java
@@ -31,6 +31,26 @@ public class alternativeelement {
         return ans;
     }
 
+    public static int[] alternativemax(int arr2[], int n) {
+        int ans[] = new int[n];
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int maxi = 0, mini = 1;
+        for (int i = 0; i < n; i++) {
+            if (arr2[i] > max) {
+                max = Math.max(max, arr2[i]);
+                ans[maxi] = arr2[i];
+                maxi = maxi + 2;
+            }
+            if (arr2[i] < min) {
+                min = Math.min(min, arr2[i]);
+                ans[mini] = arr2[i];
+                mini = mini + 2;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3, -4, -1, -6 };
         int arr1[] = { 1, 2, 3, 4, 5, 6 };
@@ -41,6 +61,11 @@ public class alternativeelement {
         }
         for (int i = 0; i < ans1.length; i++) {
             System.out.println(ans1[i] + " ");
+        }
+        int arr2[] = { 1, 2, 3, 4, 5, 6 };
+        int ans2[] = alternativemax(arr2, arr2.length);
+        for (int i = 0; i < ans2.length; i++) {
+            System.out.println(ans2[i] + " ");
         }
     }
 }
