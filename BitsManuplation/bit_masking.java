ee0c3f8c175f128413ff6b8fbe0a71ef8127400c
prince kumar <agarwal8789@gmail.com>
2023-11-05 18:54:51 +0530

    commit made

--- BitsManuplation/bit_masking.java
+++ BitsManuplation/bit_masking.java
@@ -1,17 +1,55 @@
 package BitsManuplation;
-import java.util.*;
+
 public class bit_masking {
     public static void main(String args[]) {
-        Scanner s=new Scanner(System.in);
-        System.out.println("Enter the number");
-        int x=s.nextInt();
-        int i=s.nextInt();
-        int mask=1<<i;
-        if((x&mask)==0){
-              System.out.println(0);
+        int n = 5;
+        int i = 2;
+        System.out.println(bitmasking(i));
+        System.out.println(checkingithbits(n, i));
+        System.out.println(setithbits(n, i));
+        System.out.println(clearbits(n, i));
+        System.out.println(updatebits(n, i, 1));
+        System.out.println(clearLastIbits(n, i));
+        System.out.println(togglingbits(n, i));
+    }
+
+    public static int bitmasking(int i) {
+        int mask = 1 << i;
+        return mask;
+    }
+
+    public static int checkingithbits(int n, int i) {
+        int mask = bitmasking(i);
+        if ((n & mask) == 0) {
+            return 0;
+        } else {
+            return 1;
         }
-        else{
-            System.out.println(1);
-        }
+    }
+
+    public static int setithbits(int n, int i) {
+        int mask = bitmasking(i);
+        return n | mask;
+    }
+
+    public static int clearbits(int n, int i) {
+        int mask = ~bitmasking(i);
+        return n & mask;
+    }
+
+    public static int updatebits(int n, int i, int v) {
+        int mask = ~bitmasking(i);
+        int clear = n & mask;
+        return clear | (v << i);
+    }
+
+    public static int clearLastIbits(int n, int i) {
+        int mask = ~(-1 << i);
+        return n & mask;
+    }
+
+    public static int togglingbits(int n, int i) {
+        int mask = bitmasking(i);
+        return n ^ mask;
     }
 }
