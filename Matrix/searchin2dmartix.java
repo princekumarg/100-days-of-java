043b2cf55d7c6d3764246edd931496847021b545
prince kumar <agarwal8789@gmail.com>
2023-11-13 11:17:50 +0530

    commt made

---
+++ Matrix/searchin2dmartix.java
@@ -0,0 +1,52 @@
+package Matrix;
+
+public class searchin2dmartix {
+    public static boolean binarysearch(int arr[], int x) {
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == x) {
+                return true;
+            } else if (arr[mid] > x) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+
+    public static boolean search(int matrix[][], int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        for (int i = 0; i < n; i++) {
+            boolean ans = binarysearch(matrix[i], target);
+            if (ans == true) {
+                return true;
+            }
+        }
+        return true;
+    }
+
+    public static boolean search2(int matrix[][], int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int row = 0, col = m - 1;
+        while (row < n && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } };
+        int target = 3;
+        System.out.println(search(matrix, target));
+    }
+}
