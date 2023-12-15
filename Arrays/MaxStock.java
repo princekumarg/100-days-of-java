5b349914821a0cedc0add16236f69a9ef6d93459
prince kumar <agarwal8789@gmail.com>
2023-12-15 10:52:26 +0530

    commit made

--- Arrays/MaxStock.java
+++ Arrays/MaxStock.java
@@ -11,6 +11,21 @@ public class MaxStock {
         return profit;
     }
 
+    public static int maxProfit(int arr[]) {
+        int min = 0;
+        int diff = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            int maxprofit = arr[i] - min;
+            if (maxprofit > diff) {
+                diff = maxprofit;
+            }
+            if (arr[i] < min) {// to find min
+                min = arr[i];
+            }
+        }
+        return diff;
+    }
+
     public static int maxstock1(int arr[], int n) {
         int max = arr[0], min = arr[0];
         for (int i = 0; i < n; i++) {
